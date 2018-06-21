79db71424d379673a77a7e828738a5ab5f611f83
Barry Commins <barry.commins@gmail.com>
2018-06-21 22:59:05 +0100

    Updated to support Spring Boot 2 and Spring Cloud Finchley

--- src/test/java/at/twinformatics/eureka/adapter/consul/controller/ServiceControllerTest.java
+++ src/test/java/at/twinformatics/eureka/adapter/consul/controller/ServiceControllerTest.java
@@ -41,7 +41,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.netflix.rx.SingleReturnValueHandler;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -85,8 +84,7 @@ public class ServiceControllerTest {
         executorService1 = Executors.newSingleThreadExecutor();
         serviceChangeDetector.reset();
 
-        mockMvc = MockMvcBuilders.standaloneSetup(controller)
-                .setCustomReturnValueHandlers(new SingleReturnValueHandler()).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @After
